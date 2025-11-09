0b4dfd7c35fecc0fe553d7c93608feec76a87e61
BuwieSit <santosbuhawie28@gmail.com>
2025-11-09 23:56:26 +0800

    router links

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -5,6 +5,8 @@ export default function Login() {
     return (
         <>
             <h1 class=".font-mono .text-green-500 .font-bold">Hello this is the login screen</h1>
+            <Link to="/">Home</Link>
+            <Link to="/register">Register</Link>
         </>
     )
 }
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -5,6 +5,8 @@ export default function Register() {
     return (
         <>
             <h1 class=".font-mono .text-green-500 .font-bold">Hello this is the register screen</h1>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
         </>
     )
 }
